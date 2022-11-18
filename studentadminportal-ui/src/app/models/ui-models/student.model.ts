d70f8b943fda682577151c7d356cdf25eba3edad
manaperr <manaperr@gmail.com>
2022-11-18 16:21:25 +0500

    AdminStudent

---
+++ studentadminportal-ui/src/app/models/ui-models/student.model.ts
@@ -0,0 +1,15 @@
+import { Address } from "./address.mode";
+import { Gender } from "./gender.model";
+
+export interface Student{
+  id:string,
+  firstname: string,
+  lastName:string,
+  dateOfBirth:string,
+  email:string,
+  mobile:number,
+  profileImageUrl:string,
+  genderId:string,
+  gender: Gender,
+  address: Address
+}
